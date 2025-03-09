6acc0a9f4e5191b98bb8053015e5689e9a7a75a8
Gustavo <gugufonceca.2004@gmail.com>
2025-03-09 10:42:40 -0300

    Enviando

--- gerador_de_cpf.py
+++ gerador_de_cpf.py
@@ -1,5 +1,10 @@
-cpf_enviado_usuario = '74682489070'
-nove_digitos = cpf_enviado_usuario[:9]
+import random
+
+nove_digitos = ''
+
+for i in range(9):
+    nove_digitos += str(random.randint(0,9))
+
 contador_regressivo_1 = 10
 
 resultado_digito_1 = 0
@@ -20,8 +25,8 @@ digito_2 = (resultado_digito_2 * 10) % 11
 digito_2 = digito_2 if digito_2 <= 9 else 0
 
 cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
-
-if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
-    print(f'{cpf_enviado_usuario} é válido')
-else:
-    print('CPF inválido')
+print(cpf_gerado_pelo_calculo)
+# if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
+#     print(f'{cpf_enviado_usuario} é válido')
+# else:
+#     print('CPF inválido')
